35fa3018314b393c52831cf0b43dc761d4cc6444
yasseinbilal <yassein.bilal@gmail.com>
2024-08-02 18:52:23 +0200

    update time based trigger url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const RELAYER_BASE_URL = 'https://relayer-ashy.vercel.app'
 export const TIME_BASED_TRIGGER_URL =
-  'https://triggers-one.vercel.app/triggers/time-based'
+  'https://time-based-trigger-app-m4r5f.ondigitalocean.app/triggers/time-based'
 export const EVENT_BASED_TRIGGER_URL =
   'https://triggers-one.vercel.app/triggers/event-based'
