0515b9d193b7714a94798c0d63c7b6f1f5eadca5
yasseinbilal <yassein.bilal@gmail.com>
2024-09-10 12:28:30 +0200

    add support for smart session validator

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,9 @@ export const TIME_BASED_TRIGGER_URL =
   'https://time-based-trigger-app-m4r5f.ondigitalocean.app/triggers/time-based'
 export const EVENT_BASED_TRIGGER_URL =
   'https://triggers-one.vercel.app/triggers/event-based'
+
+export const ALLOWED_VALIDATORS = {
+  OWNABLE_VALIDATOR: '0x6605F8785E09a245DD558e55F9A0f4A508434503',
+  SMART_SESSION_VALIDATOR: '0x8eeCa74941C7d81E64c1e1728bf4a3Ed174FB766',
+  MOCK_VALIDATOR: '0x503b54Ed1E62365F0c9e4caF1479623b08acbe77',
+} as const
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { ClientParams } from './types'
 import { Automation } from './Automation'
 
+export { ALLOWED_VALIDATORS } from './constants'
+
 export function createAutomationClient(params: ClientParams): Automation {
   return new Automation(params)
 }
